23119fd6604fefc6980e351eec95b5390eb389a4
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-13 16:46:32 -0500

    Update headers to bypass Amazon bot detection

--- util.py
+++ util.py
@@ -4,6 +4,9 @@ from urllib import parse
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch, br",
+    "Accept-Language": "en-US,en;q=0.8",
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'
 }
 
